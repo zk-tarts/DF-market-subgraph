acee44b05a8cc80f6e108ca0049efbc07d235921
zk-FARTs <myfulllegalname@protonmail.com>
2021-07-03 01:54:15 +0000

    fixes!

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,7 +2,7 @@ import { ListingUpdate } from "../generated/Market/Market"
 import { ListingChange } from "../generated/schema"
 
 export function handleListingUpdate(event: ListingUpdate): void {
-  let id = event.params.token
+  let id = event.transaction.hash.toHex() + event.logIndex.toString();
   let update = new ListingChange(id);
   update.price = event.params.price
   update.save()
