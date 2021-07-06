47ca874cca7e471a8bedbbdee6813d2140ca31ba
zk-FARTs <myfulllegalname@protonmail.com>
2021-07-06 15:28:46 +0000

    fixed for real maybe

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,19 +1,7 @@
-import { Address, BigInt, Bytes, ethereum, log, store } from "@graphprotocol/graph-ts";
+import { Address, Bytes, ethereum, log, store } from "@graphprotocol/graph-ts";
 import { Fee, ListedToken } from "../generated/schema";
 import { Market } from "../generated/Market/Market"
 
-
-export function handleBlock(block: ethereum.Block): void {
-  if (block.number.gt(BigInt.fromI32(16895823))){
-    let market = Market.bind(Address.fromString("0x3Fb840EbD1fFdD592228f7d23e9CA8D55F72F2F8"))
-    let fee = Fee.load(market.endDate().toString())
-    if (fee === null){
-      fee = new Fee(market.endDate().toString())
-      fee.fee = market.fee()
-      fee.save()
-    }
-  }
-}
 
 export function handleBuy(tx: ethereum.Transaction): void {
   let id = tx.hash.toHex()
@@ -26,14 +14,21 @@ export function handleUnist(tx: ethereum.Transaction): void {
 }
 
 export function handleList(tx: ethereum.Transaction): void {
+  {
+    let market = Market.bind(Address.fromString("0x3Fb840EbD1fFdD592228f7d23e9CA8D55F72F2F8"))
+    let fee = Fee.load(market.endDate().toString())
+    if (fee === null){
+      fee = new Fee(market.endDate().toString())
+      fee.fee = market.fee()
+      fee.save()
+    }
+  }
   let id = tx.hash.toHex()
   log.info('handleList with tx hash {}',[id])
-  let input = new Uint8Array(tx.input.byteLength-4) as Bytes
-  for (let i =0; i<tx.input.byteLength-4;i++){   // remove function selector
-    input[i] = tx.input[i]
-  }
-  let decodedInput = ethereum.decode("(uint256,uint256)",input) // abi decode
   log.debug('input before decode: {}', [tx.input.toHex()])
+  let input = tx.input.subarray(3) as Bytes // remove first 4 bytes: function selector
+  log.debug('input after slice: {}', [input.toHex()])
+  let decodedInput = ethereum.decode("(uint256,uint256)",input) // abi decode
   log.debug('decoded minus function selector: {} , {}', decodedInput.toStringArray())
   let token = ListedToken.load(id)
   token = new ListedToken(id)
