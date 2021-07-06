c50de3dd1c96a2740ac2ab29a5c8540834e7fb60
zk-FARTs <myfulllegalname@protonmail.com>
2021-07-06 23:12:13 +0000

    correctly show/remove listed

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,17 +2,17 @@ import { store } from "@graphprotocol/graph-ts";
 import { ListedToken } from "../generated/schema";
 import { BuyCall, ListCall, UnlistCall } from "../generated/Market/Market"
 export function handleBuy(tx: BuyCall): void {
-  let id = tx.transaction.hash.toHex()
+  let id = tx.inputs.tokenID.toHexString()
   store.remove('ListedToken',id)
 }
 
 export function handleUnist(tx: UnlistCall): void {
-  let id = tx.transaction.hash.toHex()
+  let id = tx.inputs.id.toHexString()
   store.remove('ListedToken',id)
 }
 
 export function handleList(tx: ListCall): void {
-  let id = tx.transaction.hash.toHex()
+  let id = tx.inputs.tokenID.toHexString()
   let token = new ListedToken(id)
   token.tokenID = tx.inputs.tokenID
   token.owner = tx.from.toHexString()
