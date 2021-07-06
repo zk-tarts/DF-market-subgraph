824c1c3ad90e3e14f0a61ddee49b1e07da49fac3
zk-FARTs <myfulllegalname@protonmail.com>
2021-07-06 17:29:27 +0000

    i try again to fix memory errors

--- src/mapping.ts
+++ src/mapping.ts
@@ -13,17 +13,15 @@ export function handleUnist(tx: ethereum.Transaction): void {
 
 export function handleList(tx: ethereum.Transaction): void {
   let id = tx.hash.toHex()
-  log.error('handleList with tx hash {}',[id])
-  log.error('input before decode: {}', [tx.input.toHex()])
-  let input = tx.input.subarray(3) as Bytes // remove first 4 bytes: function selector
-  log.error('input after slice: {}', [input.toHex()])
-  let decodedInput = ethereum.decode("(uint256,uint256)",input) // abi decode
-  log.error('decoded minus function selector: {} , {}', decodedInput.toStringArray())
   let token = ListedToken.load(id)
   token = new ListedToken(id)
-  token.tokenID = decodedInput.toBigIntArray()[0]
-  token.owner = tx.from.toHexString()
-  token.price = decodedInput.toBigIntArray()[1]
+  {
+    let input = tx.input.subarray(3) as Bytes // remove first 4 bytes: function selector
+    let decodedInput = ethereum.decode("(uint256,uint256)",input) // abi decode
+    token.tokenID = decodedInput.toBigIntArray()[0]
+    token.owner = tx.from.toHexString()
+    token.price = decodedInput.toBigIntArray()[1]
+  }
   token.save()
 }
 
