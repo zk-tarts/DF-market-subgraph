49e3304e4c186f18528ea7b69c75d302e122a843
zk-FARTs <myfulllegalname@protonmail.com>
2021-07-06 00:32:31 +0000

    another attempt at fixing

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { Address, BigInt, Bytes, ethereum, log } from "@graphprotocol/graph-ts";
+import { Address, BigInt, Bytes, ethereum, log, store } from "@graphprotocol/graph-ts";
 import { Fee, ListedToken } from "../generated/schema";
 import { Market } from "../generated/Market/Market"
 
@@ -7,23 +7,33 @@ export function handleBlock(block: ethereum.Block): void {
   if (block.number.gt(BigInt.fromI32(16895823))){
     let market = Market.bind(Address.fromString("0x3Fb840EbD1fFdD592228f7d23e9CA8D55F72F2F8"))
     let fee = Fee.load(market.endDate().toString())
-    if (fee == null){
+    if (fee === null){
       fee.fee = market.fee()
       fee.save()
     }
   }
 }
 
+export function handleBuy(tx: ethereum.Transaction): void {
+  let id = tx.hash.toHex()
+  store.remove('ListedToken',id)
+}
+
+export function handleUnist(tx: ethereum.Transaction): void {
+  let id = tx.hash.toHex()
+  store.remove('ListedToken',id)
+}
 
 export function handleList(tx: ethereum.Transaction): void {
   let id = tx.hash.toHex()
   log.info('handleList with tx hash {}',[id])
-  let x = new Uint8Array(tx.input.byteLength-4) as Bytes
+  let input = new Uint8Array(tx.input.byteLength-4) as Bytes
   for (let i =0; i<tx.input.byteLength-4;i++){   // remove function selector
-    x[i] = tx.input[i]
+    input[i] = tx.input[i]
   }
-  let decodedInput = ethereum.decode("(uint256,uint256)",x) // abi decode
-  log.debug('{} , {}', decodedInput.toStringArray())
+  let decodedInput = ethereum.decode("(uint256,uint256)",input) // abi decode
+  log.debug('input before decode: {}', [tx.input.toHex()])
+  log.debug('decoded minus function selector: {} , {}', decodedInput.toStringArray())
   let token = ListedToken.load(id)
   token = new ListedToken(id)
   token.tokenID = decodedInput.toBigIntArray()[0]
