6f15e040d39d465b4f30c07d374dce0ccaa26c57
zk-FARTs <myfulllegalname@protonmail.com>
2021-07-06 17:21:45 +0000

    remove fee, logs now error for easier debug

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 
 
 Should show  
-  - the fees for each round 
   - the tokens listed and their prices and owners
-  
+  - 
 
 check it out at https://thegraph.com/explorer/subgraph/zk-farts/dfartifactmarket
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,7 +1,5 @@
-import { Address, Bytes, ethereum, log, store } from "@graphprotocol/graph-ts";
-import { Fee, ListedToken } from "../generated/schema";
-import { Market } from "../generated/Market/Market"
-
+import { Bytes, ethereum, log, store } from "@graphprotocol/graph-ts";
+import { ListedToken } from "../generated/schema";
 
 export function handleBuy(tx: ethereum.Transaction): void {
   let id = tx.hash.toHex()
@@ -14,22 +12,13 @@ export function handleUnist(tx: ethereum.Transaction): void {
 }
 
 export function handleList(tx: ethereum.Transaction): void {
-
-  let market = Market.bind(Address.fromString("0x3Fb840EbD1fFdD592228f7d23e9CA8D55F72F2F8"))
-  let fee = Fee.load(market.endDate().toString())
-  if (fee === null){
-    fee = new Fee(market.endDate().toString())
-    fee.fee = market.fee()
-    fee.save()
-  }
-
   let id = tx.hash.toHex()
-  log.info('handleList with tx hash {}',[id])
-  log.debug('input before decode: {}', [tx.input.toHex()])
+  log.error('handleList with tx hash {}',[id])
+  log.error('input before decode: {}', [tx.input.toHex()])
   let input = tx.input.subarray(3) as Bytes // remove first 4 bytes: function selector
-  log.debug('input after slice: {}', [input.toHex()])
+  log.error('input after slice: {}', [input.toHex()])
   let decodedInput = ethereum.decode("(uint256,uint256)",input) // abi decode
-  log.debug('decoded minus function selector: {} , {}', decodedInput.toStringArray())
+  log.error('decoded minus function selector: {} , {}', decodedInput.toStringArray())
   let token = ListedToken.load(id)
   token = new ListedToken(id)
   token.tokenID = decodedInput.toBigIntArray()[0]
